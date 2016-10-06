e35d9aac553de5767f72aeef9ae8df5b2447f329
Dawid <dyrcu15@gmail.com>
2016-10-06 13:01:32 +0200

    Layout structure

--- Weather.Droid/Weather.Droid/Resources/Resource.Designer.cs
+++ Weather.Droid/Weather.Droid/Resources/Resource.Designer.cs
@@ -57,6 +57,25 @@ namespace Weather.Droid
 			}
 		}
 		
+		public partial class Id
+		{
+			
+			// aapt resource value: 0x7f050001
+			public const int imageView1 = 2131034113;
+			
+			// aapt resource value: 0x7f050000
+			public const int textView1 = 2131034112;
+			
+			static Id()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Id()
+			{
+			}
+		}
+		
 		public partial class Layout
 		{
 			
